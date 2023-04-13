5236f748d970079e8064cd2077666971cd505b12
algailoc <algailoc@gmail.com>
2023-04-13 22:04:52 +0300

    Продолжила прописывать блок активностей

--- lib/presentation/bloc/activities_bloc/activities_bloc.dart
+++ lib/presentation/bloc/activities_bloc/activities_bloc.dart
@@ -14,86 +14,112 @@ class ActivitiesBloc extends Bloc<ActivitiesEvent, ActivitiesState> {
   final _pickedActivities = <ActivityEntity>[];
   // TODO: assign this variable in init
   bool _isCreate = false;
-  Mood _oldMood = Mood.awful;
+  Mood _oldMood = Mood.mediocre;
   // TODO: need event for changing mood
-  Mood _newMood = Mood.good;
+  Mood _newMood = Mood.mediocre;
 
-  ActivitiesBloc() : super(ActivitiesInitial()) {
+  ActivitiesBloc() : super(const ActivitiesInitial([])) {
     on<ActivitiesEvent>((event, emit) async {
-      // TODO: implement event handler
+      if (event is InitActivitiesBlocEvent) {
+        emit(ActivitiesPendingState(activities));
+
+        _isCreate = event.isCreate;
+        _oldMood = event.originalMood;
+        _newMood = _oldMood;
+
+        // TODO: load activities
+
+        emit(ActivitiesLoadedState(activities));
+      }
 
       //////////////////////////////
       // if event is save day /////
       /////////////////////////////
 
-      final listToUpdate = <ActivityEntity>[];
+      else if (event is SaveDayEvent) {
+        final listToUpdate = <ActivityEntity>[];
 
-      if (_oldMood != _newMood) {
-        final listToUpdate = _pickedActivities
-            .map(
-              (e) => e.changeRating(
-                oldMood: _oldMood,
-                newMood: _newMood,
-              ),
+        if (_oldMood != _newMood && !_isCreate) {
+          final listToUpdate = _pickedActivities
+              .map(
+                (e) => e.changeRating(
+                  oldMood: _oldMood,
+                  newMood: _newMood,
+                ),
+              )
+              .toList();
+
+          //TODO: then make call to update these activities
+
+          listToUpdate.clear();
+        }
+
+        final removedActivities = activities
+            .where(
+              (e) => _removedActivitiesIds.contains(e.id),
             )
             .toList();
+        for (var activity in removedActivities) {
+          final newActivity = activity.removeRating(_newMood);
+          listToUpdate.add(newActivity);
+        }
 
-        // then make call to update these activities
+        final addedActivities = activities
+            .where(
+              (e) => _addedActivitiesIds.contains(e.id),
+            )
+            .toList();
+        for (var activity in addedActivities) {
+          final newActivity = activity.addRating(_newMood);
+          listToUpdate.add(newActivity);
+        }
 
-        listToUpdate.clear();
+        //TODO: then make call to update these activities
+
+        emit(ActivitiesLoadedState(activities));
       }
-
-      final removedActivities = activities
-          .where(
-            (e) => _removedActivitiesIds.contains(e.id),
-          )
-          .toList();
-      for (var activity in removedActivities) {
-        final newActivity = activity.removeRating(_newMood);
-        listToUpdate.add(newActivity);
-      }
-
-      final addedActivities = activities
-          .where(
-            (e) => _addedActivitiesIds.contains(e.id),
-          )
-          .toList();
-      for (var activity in addedActivities) {
-        final newActivity = activity.addRating(_newMood);
-        listToUpdate.add(newActivity);
-      }
-
-      // then make call to update these activities
 
       ////////////////////////////////////
       // if event is choose activity /////
       ////////////////////////////////////
 
-      if (_removedActivitiesIds.contains('event.id')) {
-        _removedActivitiesIds.remove('event.id');
-      } else {
-        _addedActivitiesIds.add('event.id');
+      else if (event is SelectActivityEvent) {
+        if (_removedActivitiesIds.contains(event.activityId)) {
+          _removedActivitiesIds.remove(event.activityId);
+        } else {
+          _addedActivitiesIds.add(event.activityId);
+        }
+
+        emit(ActivitiesLoadedState(activities));
       }
 
       //////////////////////////////////////
       // if event is unchoose activity /////
       //////////////////////////////////////
 
-      if (_addedActivitiesIds.contains('event.id')) {
-        _addedActivitiesIds.remove('event.id');
-      } else {
-        _removedActivitiesIds.add('event.id');
+      else if (event is UnselectActivityEvent) {
+        if (_addedActivitiesIds.contains(event.activityId)) {
+          _addedActivitiesIds.remove(event.activityId);
+        } else {
+          _removedActivitiesIds.add(event.activityId);
+        }
+
+        emit(ActivitiesLoadedState(activities));
       }
 
       //////////////////////////////////////
       // if event is update activity ///////
       //////////////////////////////////////
 
-      final index =
-          activities.indexWhere((element) => element.id == 'event.id');
-      if (index > -1) {
-        // call for update
-        activities[index] = activities[index].copyWith(name: 'event.name');
+      else if (event is ChangeActivityNameEvent) {
+        final index =
+            activities.indexWhere((element) => element.id == event.activityId);
+        if (index > -1) {
+          //TODO: call for update
+          activities[index] = activities[index].copyWith(name: event.newName);
+        }
+
+        emit(ActivitiesLoadedState(activities));
       }
     });
   }
--- lib/presentation/bloc/activities_bloc/activities_state.dart
+++ lib/presentation/bloc/activities_bloc/activities_state.dart
@@ -1,10 +1,31 @@
 part of 'activities_bloc.dart';
 
 abstract class ActivitiesState extends Equatable {
-  const ActivitiesState();
-  
+  final List<ActivityEntity> activities;
+
+  const ActivitiesState(this.activities);
+
   @override
   List<Object> get props => [];
 }
 
-class ActivitiesInitial extends ActivitiesState {}
+class ActivitiesInitial extends ActivitiesState {
+  const ActivitiesInitial(super.activities);
+}
+
+class ActivitiesLoadedState extends ActivitiesState {
+  const ActivitiesLoadedState(super.activities);
+}
+
+class ActivitiesPendingState extends ActivitiesState {
+  const ActivitiesPendingState(super.activities);
+}
+
+class ActivitiesErrorState extends ActivitiesState {
+  final String error;
+
+  const ActivitiesErrorState(
+    super.activities, {
+    required this.error,
+  });
+}
